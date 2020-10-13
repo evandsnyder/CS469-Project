352c19862b1ebe589743c8731af26e56d2642b68
Th3Arch1t3ct <arch1t3ct.117@gmail.com>
2020-10-13 13:57:41 -0400

    Updated readme, removed debug_network

--- README.md
+++ README.md
@@ -6,9 +6,61 @@ Evan Snyder
 
 We've chosen prompt 2 for our distributed systems assignment
 
+## Installation
+There are several tools included to get the application up and running. Follow along to install and configure the tool.
+
+##### Required tools and Packages
+* python3
+* CMake
+* libgtk-3-dev
+* sqlite3
+* libssl-dev
+
+The first thing to do is to create a simple database to be used. A python script as been included to facilitate this process:
+```
+python3 create_db.py  
+```
+This will produce an `items.db` file that can be used for the item database tool.
+
+At this point, we can build all of the tools using CMake:
+```
+cmake .
+make
+```
+We still need to make a user account for login purposes. A tool has also been provided for this purpose:
+```
+./user_mgr username password
+```
+This will store the username and encrypted password into the database.
+
+Finally, a server key and certificate must be available to encrypt the network traffic. This can be done via:
+```
+openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out cert.pem
+```
+
+**At this point, all applications are ready to be run.**
+
+### Configurations
+
+The server application takes command line arguments as well as a config file to prepare the server. 
+Command line arguments may appear as:
+```
+./server -l 4466 -s localhost -p 6644 -k key.pem -c server.conf -d items.db -i 1:H
+```
+
+A Sample config file may look like:
+```
+PORT=4466
+BACKUP_SERVER=localhost
+BACKUP_PORT=6644
+DATABASE=items.db
+INTERVAL=24:m
+```
+
+
 #### TODO:
 * ~~Client Login UI~~
-* Client Main UI
+* ~~Client Main UI~~
 * ~~Client Connect to server~~
 * ~~Server argument parsing~~
 * ~~Server SSL/TLS Listening~~
--- client/client.c
+++ client/client.c
@@ -4,12 +4,7 @@
 #include "login_window.h"
 #include "network.h"
 
-void DEBUG_network();
-
 int main(int argc, char** argv){
-    printf("Hello from client!\n");
-
-    // DEBUG_network();
 
     gtk_init(&argc, &argv);
 
@@ -17,106 +12,4 @@ int main(int argc, char** argv){
     gtk_main();
 
     return 0;
-}
-//
-void DEBUG_network() {
-    char buffer[BUFFER_SIZE];
-    if (database_connect(DEFAULT_SERVER, DEFAULT_SERVER_PORT) < 0) {
-        fprintf(stderr, "Could not connect to server\n");
-    }
-
-    //////
-    // auth
-    bzero(buffer, BUFFER_SIZE);
-    //sprintf(buffer, "AUTH %s %s", "esnyder", "password");
-    sprintf(buffer, "AUTH %s %s", "esnyder", "abcd123");
-    // sprintf(buffer, "SYNC");
-
-    SSL_write(ssl, buffer, strlen(buffer));
-
-    bzero(buffer, BUFFER_SIZE);
-    SSL_read(ssl, buffer, BUFFER_SIZE);
-
-    fprintf(stdout, "MSG Received: %s\n", buffer);
-
-    //////
-    // test add/mod/del
-    Item test_item;
-    //test_item.id=-1;
-    test_item.id=149;
-    test_item.name="test item";
-    test_item.armor=42;
-    test_item.health=75;
-    test_item.mana=3;
-    test_item.sellPrice=123;
-    test_item.damage=321;
-    test_item.critChance=0.05;
-    test_item.range=100;
-    test_item.description="things and stuff";
-
-    bzero(buffer, BUFFER_SIZE);
-    //sprintf(buffer, "DEL %d", 150);
-    sprintf(buffer, "MOD %s", serialize_item(&test_item, NULL));
-    SSL_write(ssl, buffer, strlen(buffer));
-    bzero(buffer, BUFFER_SIZE);
-    SSL_read(ssl, buffer, BUFFER_SIZE);
-    fprintf(stdout, "MSG Received: %s\n", buffer);
-    //return;
-
-    //////
-    // get all and print results
-    bzero(buffer, BUFFER_SIZE);
-    sprintf(buffer, "GET ALL");
-    SSL_write(ssl, buffer, strlen(buffer));
-
-    int rcount = 1;
-    fprintf(stdout, "Message Received: ");
-    // Need to build one giant string
-    size_t cur_size = 1024 * 4;
-    size_t mem_size = 1024 * 4;
-    char *allItems = (char *) malloc(sizeof(char) * mem_size);
-    allItems[0] = '\0'; // Make it empty
-    while (rcount > 0) {
-        bzero(buffer, BUFFER_SIZE);
-        rcount = SSL_read(ssl, buffer, BUFFER_SIZE);
-        if (strlen(allItems) + strlen(buffer) >= cur_size) {
-            cur_size += mem_size;
-            allItems = realloc(allItems, cur_size);
-        }
-        strncat(allItems, buffer, strlen(buffer));
-
-        fprintf(stdout, "%s\n", buffer);
-        if (buffer[strlen(buffer) - 1] == GROUP_SEPARATOR)
-            rcount = 0;
-
-    }
-    fprintf(stdout, "\nEnd Message\n\n");
-
-    char* token;
-    char str2[] = {RECORD_SEPARATOR, '\0'};
-    token = strtok(allItems, str2);
-
-    Item *items[MAX_ITEMS] = {0};
-    int available = 0;
-    Item *newItem;
-
-    while(token != NULL){
-        newItem = (Item*)malloc(sizeof(Item));
-        deserialize_item(token, newItem);
-        items[available++] = newItem;
-        token = strtok(NULL, str2);
-    }
-
-    for(int i =0; i < available; i++){
-        Item* cur = items[i];
-        fprintf(stdout, "%s {%d, %d, %d, %d, %d, %f, %d, %s}\n",
-                cur->name, cur->armor, cur->health, cur->mana,
-                cur->sellPrice, cur->damage, cur->critChance, cur->range,
-                cur->description);
-    }
-
-    for(int i  = 0; i < available; i++){
-        if(items[i] != NULL)
-            free(items[i]);
-    }
 }
